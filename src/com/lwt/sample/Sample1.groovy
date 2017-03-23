14fe96b75b2aa54b0a31ff9c5db4a7b98922121b
lekoxnfx <lekoxnfx@qq.com>
2017-03-23 09:30:30 +0800

    sample to invoke another plugin in N4

--- src/com/lwt/sample/Sample1.groovy
+++ src/com/lwt/sample/Sample1.groovy
@@ -11,22 +11,20 @@ class Sample1 {
     public void test1(){
         String className = "Class2"
         //better use try-catch
-        //Way1:java-style
-        def classInstance1 = groovyApi.getGroovyClassInstance(className)
-        String res1 = classInstance1.invokeMethod("doTest",null)
-        //Way2:groovy-style，需要import Class2
+
+        //Way1:需要import Class2
         Class2 classInstance2 = (Class2)groovyApi.getGroovyClassInstance(className)
-        String res2 = classInstance2.doTest()
+        String res2 = classInstance2.doTest("para1")
         //way3:无法import Class2
         def classInstance3 = groovyApi.getGroovyClassInstance(className)
-        String res3 = classInstance3.doTest()//不要在意下划线
+        String res3 = classInstance3.doTest("para1")//不要在意下划线
 
 
     }
 
 
     class Class2{
-        public String doTest(){
+        public String doTest(String para1){
             return "test"
         }
     }
